958188df7428d8e025bfa006fb12affe622ab402
denny <suxiaopo137@foxmail.com>
2022-05-17 00:59:42 +0800

    style: 首页样式修改

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,26 @@
 </template>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-size: 10px;
+  font-family: Microsoft YaHei, Microsoft YaHei-Normal;
+  background: #eee;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
